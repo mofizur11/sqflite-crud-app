428f4cf2874f8445d89db417fd487e729de38f39
Mofizur Rahman <mofizur853@gmail.com>
2022-08-28 00:07:57 +0600

    crate, read, update and deleted

--- lib/demo.dart
+++ lib/demo.dart
@@ -1,65 +1,100 @@
 import 'package:flutter/material.dart';
-import 'package:sqflite_crud/add_contacts.dart';
-import 'package:sqflite_crud/contact.dart';
-import 'package:sqflite_crud/db_helper.dart';
+import 'package:sqflite_crud/add_contact.dart';
+import 'package:sqflite_crud/contacts.dart';
+import 'package:sqflite_crud/helper.dart';
+
 
 class Demo extends StatefulWidget {
   const Demo({Key? key}) : super(key: key);
 
   @override
-  State<Demo> createState() => _DemoState();
+  _DemoState createState() => _DemoState();
 }
 
 class _DemoState extends State<Demo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Local DB')),
+      appBar: AppBar(
+        title: const Text('Flutter SQLite'),
+        centerTitle: true,
+      ),
+      //add Future Builder to get contacts
       body: FutureBuilder<List<Contact>>(
-          future: DBHelper.readContact(),
-          builder:
-              (BuildContext context, AsyncSnapshot<List<Contact>> snapshot) {
-            if (!snapshot.hasData) {
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    CircularProgressIndicator(),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Text('Loading ......')
-                  ],
-                ),
-              );
-            }
-            return snapshot.data!.isEmpty
-                ? const Center(
-                    child: Text('Vai data nai '),
-                  )
-                : ListView(
-                    children: snapshot.data!.map((contacts) {
-                      return Center(
-                        child: Card(
-                            elevation: 10,
-                            child: ListTile(
-                              title: Text('${contacts.name}'),
-                              subtitle: Text('${contacts.contact}'),
-                            )),
-                      );
-                    }).toList(),
-                  );
-          }),
+        future: DBHelper.readContacts(), //read contacts list here
+        builder: (BuildContext context, AsyncSnapshot<List<Contact>> snapshot) {
+          //if snapshot has no data yet
+          if (!snapshot.hasData) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  CircularProgressIndicator(),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text('Loading...'),
+                ],
+              ),
+            );
+          }
+          //if snapshot return empty [], show text
+          //else show contact list
+          return snapshot.data!.isEmpty
+              ? const Center(
+                  child: Text('No Contact in List yet!'),
+                )
+              : ListView(
+                  children: snapshot.data!.map((contacts) {
+                    return Center(
+                      child: ListTile(
+                        title: Text(contacts.name),
+                        subtitle: Text(contacts.contact),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.delete),
+                          onPressed: () async {
+                            await DBHelper.deleteContacts(contacts.id!);
+                            setState(() {
+                              //rebuild widget after delete
+                            });
+                          },
+                        ),
+                        onTap: () async {
+                          //tap on ListTile, for update
+                          final refresh = await Navigator.of(context)
+                              .push(MaterialPageRoute(
+                                  builder: (_) => AddContacts(
+                                        contact: Contact(
+                                          id: contacts.id,
+                                          name: contacts.name,
+                                          contact: contacts.contact,
+                                        ),
+                                      )));
+
+                          if (refresh) {
+                            setState(() {
+                              //if return true, rebuild whole widget
+                            });
+                          }
+                        },
+                      ),
+                    );
+                  }).toList(),
+                );
+        },
+      ),
       floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
         onPressed: () async {
           final refresh = await Navigator.of(context)
-              .push(MaterialPageRoute(builder: (_) => const AddContact()));
+              .push(MaterialPageRoute(builder: (_) => const AddContacts()));
 
           if (refresh) {
-            setState(() {});
+            setState(() {
+              //if return true, rebuild whole widget
+            });
           }
         },
-        child: const Icon(Icons.add),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:sqflite_crud/demo.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Demo(),
+      home: Demo(),
     );
   }
 }
